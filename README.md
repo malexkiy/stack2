7a671f01ee40fe7540a0d7910394f8f1b7a2fc45
malexkiy <malexkiy@gmail.com>
2017-10-14 22:28:57 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/malexkiy/stack2.svg?branch=master)](https://travis-ci.org/malexkiy/stack2)
+
+```
+cmake -H. -B_builds -DBUILD_EXAMPLES=ON
+cmake --build _builds
+_builds/example
+```
---
+++ tests/init.cpp
@@ -0,0 +1,71 @@
+#include <catch.hpp>
+#include <stack2.hpp>
+
+SCENARIO("stack init", "[init]") {
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+}
+
+SCENARIO("stack init object", "[init object]") {
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+
+	stack<int> st2(st1);
+
+	REQUIRE(st1.count() == 3);
+	REQUIRE(st2.count() == 3);
+}
+
+SCENARIO("stack op=", "[op=]") {
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+
+	stack<int> st2 = st1;
+
+	REQUIRE(st1.count() == 3);
+	REQUIRE(st2.count() == 3);
+}
+
+SCENARIO("stack push", "[push]") {
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+
+	st.push(6);
+	REQUIRE(st.count() == 1);
+	st.push(76);
+	REQUIRE(st.count() == 2);
+	st.push(-412);
+	REQUIRE(st.count() == 3);
+}
+
+SCENARIO("stack pop", "[push]") {
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+
+	st.push(6);
+	REQUIRE(st.count() == 1);
+
+	st.push(76);
+	REQUIRE(st.count() == 2);
+
+	st.push(-412);
+	REQUIRE(st.count() == 3);
+
+	int tmp = st.pop();
+	REQUIRE(st.count() == 2);
+	REQUIRE(tmp == -412);
+	
+	tmp = st.pop();
+	REQUIRE(st.count() == 1);
+	REQUIRE(tmp == 76);
+	
+	tmp = st.pop();
+	REQUIRE(st.count() == 0);
+	REQUIRE(tmp == 6);
+}
